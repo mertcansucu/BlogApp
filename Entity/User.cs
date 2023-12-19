fe9771c465c837d46e3bd08c46ccd1f4e753e417
mertcansucu <mrtcnscc@gmail.com>
2023-12-19 18:06:15 +0300

    entity(schema of database tables created)

---
+++ Entity/User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public List<Post> Posts { get; set; } = new List<Post>();//bir kullanıcı birden fazla post yayınlayacağı için liste şeklinde bağlantıyı sağladım
+        public List<Comment> Comments { get; set; } = new List<Comment>();//Bir kullanıı bir posta birden fazla yorum yapabildiği için liste şeklinde bağladım
+    }
+}
