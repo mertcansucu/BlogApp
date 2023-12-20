87a506383b3d2ddd7b984ba62c60482ac7027f01
mertcansucu <mrtcnscc@gmail.com>
2023-12-20 15:17:21 +0300

    MySQL Database Provider

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BlogContext>(options =>{//veritabı bağlantılarını yapacağım yeri yazdım => BlogContext.cs
     var config = builder.Configuration;
-    var connectionString = config.GetConnectionString("sql_connection");
+    var connectionString = config.GetConnectionString("mysql_connection");
     /*
         sql_connection =>  bu bilgiyi appsettings.Development.json dan aldım:
     */
-    options.UseSqlite(connectionString);
+    // options.UseSqlite(connectionString);//sqllite tan kaptıp mysql e geçtim
     //bu işlemlerden sonra BlogContext ile birleştirdiğim entityleri veritabına aktarıcam bunu migration ile yaptım
+
+    //mysql bağlantı
+    var version = new MySqlServerVersion(new Version(8,0,35));
+    options.UseMySql(connectionString,version);
 });
 
 var app = builder.Build();
